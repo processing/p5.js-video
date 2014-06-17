f7f48a06175dadbcfb6cd8939504fda9f92d642d
Daniel Shiffman <daniel@shiffman.net>
2014-06-17 12:30:40 -0400

    new examples

---
+++ examples/new/04_circle_slider/sketch.js
@@ -0,0 +1,20 @@
+// 1:13
+
+var slider;
+
+function setup() {
+  createCanvas(displayWidth,displayHeight);
+  circlecolor = color(255);
+  
+  slider = createSlider(0,255,127);
+  slider.position(width/4-56,height/4+128);
+
+}
+
+function draw() {
+  clear();
+  fill(slider.value());
+  ellipse(width/4,height/4,128,128);
+}
+
+

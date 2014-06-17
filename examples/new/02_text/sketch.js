9aa66c5da0130e5e322661071c1e185f1889b68f
Daniel Shiffman <daniel@shiffman.net>
2014-06-17 12:21:35 -0400

    cleanup

--- examples/new/02_text/sketch.js
+++ examples/new/02_text/sketch.js
@@ -1,72 +1,105 @@
-// 00:54
+// 00:36
 
-var txt = [];
-
+var shapes = [];
 
 function setup() {
-  var w = displayWidth;
-  var h = displayHeight;
-
-  var choices = [ 'hello!','p5 is fun!','ding a ling a ding dong!' ];
-
-  var interval = setInterval(function() {
-    var i = floor(random(choices.length));
-    var p = createP(choices[i]);
-    p.position(random(w),random(h/2,h));
-    txt.push(p);
-  },10);
-  
-  // Trigger with popcorn?
+  createCanvas(displayWidth, displayHeight);
   setTimeout(function() {
-    clearInterval(interval);
+    addShapes(24,width/2,width,0,height/2,'circle');
+  },0);
+  setTimeout(function() {
+    addShapes(24,0,width/2,0,height/2,'rect');
+  },500);
+  setTimeout(function() {
+    addShapes(24,width/2,width,height/2,height,'line');
+  },1000);
+  setTimeout(function() {
+    addShapes(24,0,width/2,height/2,height,'curve');
+  },1500);
+  setTimeout(function() {
+    addShapes(24,width/2-144,width/2+144,height/2-144,height/2+144,'square');
   },2000);
-
   setTimeout(function() {
-     var input = createInput('Enter some text!');
-     input.position(w/2,h/2-50);
-     
-     input.elt.oninput = function() {
-      for (var i = 0; i < txt.length; i++) {
-        txt[i].html(input.value());
-      }
-     };
-  },2000);
-
-
-  setTimeout(function() {
-    var slider = createSlider(4,72,12);
-    slider.position(w/4,h/2-50);
-     
-    slider.elt.oninput = function() {
-      for (var i = 0; i < txt.length; i++) {
-        //txt[i].size(input.elt.value);
-        txt[i].style('font-size: '+slider.value()+'pt');
-      }
-    };
+    addShapes(24,width/2-72,width/2+72,height/2-72,height/2+72,'triangle');
   },2500);
-
-  setTimeout(function() {
-    var button1 = createButton('red');
-    button1.position(w/4,h/4);
-    button1.mousePressed(function() {
-      for (var i = 0; i < txt.length; i++) {
-        txt[i].style('color: #FF0000');
-      }
-    });
-    var button2 = createButton('green');
-    button2.position(w/4+50,h/4);
-    button2.mousePressed(function() {
-      for (var i = 0; i < txt.length; i++) {
-        txt[i].style('color: #00FF00');
-      }
-    });
-    var button3 = createButton('blue');
-    button3.position(w/4+100,h/4);
-    button3.mousePressed(function() {
-      for (var i = 0; i < txt.length; i++) {
-        txt[i].style('color: #0000FF');
-      }
-    });
-  },3000);
+  setTimeout(colorize,3000);
 }
 
+function draw() {
+  clear();
+  for (var i = 0; i < shapes.length; i++) {
+    shapes[i].display();
+    shapes[i].attract(mouseX,mouseY);
+  }
+}
+
+// Trigger this from popcorn?
+function addShapes(num,minx,maxx,miny,maxy,type) {
+  for (var i = 0; i <num; i++) {
+    shapes.push(new Shape(random(minx,maxx),random(miny,maxy),type));
+  }
+}
+
+function colorize() {
+  for (var i = 0; i < shapes.length; i++) {
+    shapes[i].color = color(random(255),random(255),random(255),100);
+  }
+}
+
+function Shape(x,y,t) {
+  this.x = x || random(width);
+  this.y = y || random(height);
+  this.w = random(24,72);
+  this.h = this.w;
+  this.color = color(0,50);
+  this.type = t || 'circle';
+  if (this.type === 'rect') {
+    this.h -= this.h/2;
+  } else if (this.type === 'line' || this.type === 'curve') {
+    this.x2 = this.x + random(-72,72);
+    this.y2 = this.y + random(-72,72);
+    // Finalize
+    if (this.type === 'curve') {
+      // This is silly, need to something else
+      this.x3 = (this.x*0.25 + this.x2*0.75) + random(-12,12);
+      this.y3 = (this.y*0.25 + this.y2*0.75) + random(-12,12);
+      this.x4 = (this.x*0.75 + this.x2*0.25) + random(-12,12);
+      this.y4 = (this.y*0.75 + this.y2*0.25) + random(-12,12);
+    }
+  }
+}
+
+Shape.prototype.display = function() {
+  fill(this.color);
+  stroke(this.color);
+  rectMode(CENTER);
+  ellipseMode(CENTER);
+  if (this.type === 'rect' || this.type === 'square') {
+    rect(this.x,this.y,this.w,this.h);
+  } else if (this.type === 'circle') {
+    ellipse(this.x,this.y,this.w,this.h);
+  } else if (this.type === 'line') {
+    line(this.x,this.y,this.x2,this.y2);
+  } else if (this.type === 'curve') {
+    // Not implemented yet??
+    // bezier(this.x,this.y,this.x2,this.y2,this.x3,this.y3,this.x4,this.y4);
+    line(this.x,this.y,this.x3,this.y3);
+    line(this.x3,this.y3,this.x4,this.y4);
+    line(this.x4,this.y4,this.x2,this.y2);
+  } else if (this.type === 'triangle') {
+    beginShape();
+    vertex(this.x,this.y);
+    vertex(this.x-this.w/2,this.y+this.h);
+    vertex(this.x+this.w/2,this.y+this.h);
+    endShape(CLOSE);
+  }
+};
+
+Shape.prototype.attract = function(x,y) {
+  if (dist(x,y,this.x,this.y) < 100) {
+    this.x = lerp(this.x,x,0.05);
+    this.y = lerp(this.y,y,0.05);
+    this.x += random(-5,5);
+    this.y += random(-5,5);
+  }
+};

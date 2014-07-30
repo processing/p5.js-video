a5712e07270e7b8739ec05b2455fbbb75345f1d2
Daniel Shiffman <daniel@shiffman.net>
2014-07-30 12:14:27 -0400

    new examples #19

--- examples/circle/sketch.js
+++ examples/circle/sketch.js
@@ -1,2 +1,21 @@
-createCanvas(480,480);
-ellipse(240,240,200,200);
+
+var col;
+
+function setup() {
+  createCanvas(480, 480);
+  col = color(random(255), random(255), random(255));
+}
+
+function draw() {
+  background(255);
+  stroke(51);
+  fill(col);
+  ellipse(240,240,200,200);
+}
+
+function mousePressed() {
+  var d = dist(mouseX, mouseY, 240, 240);
+  if (d < 100) {
+    col = color(random(255), random(255), random(255));
+  }
+}

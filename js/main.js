8e657d0b46142b32bc02c676f7ce645036192e4c
Scott Garner <scott@j38.net>
2014-08-05 15:34:28 -0500

    Cleanup and minified vendor libraries

--- js/main.js
+++ js/main.js
@@ -101,21 +101,21 @@ var main = {
       chroma = seriously.effect('chroma');
 
       if (Modernizr.video.webm && Modernizr.video.h264) {
-        console.log("Chrome");
+        //console.log("Chrome");
         chroma.weight = .9;
         chroma.balance = 1;
         chroma.clipWhite = 1;
         chroma.clipBlack = 0;
         chroma.screen = [.2,1,.1,1];
       } else if (!Modernizr.video.webm && Modernizr.video.h264) {
-        console.log("Safari");
+        //console.log("Safari");
         chroma.weight = 1.25;
         chroma.balance = 1;
         chroma.clipWhite = 1;
         chroma.clipBlack = 0;        
         chroma.screen = [.3,.9,.15,1];
       } else if (Modernizr.video.webm && !Modernizr.video.h264) {
-        console.log("Firefox");
+        //console.log("Firefox");
         chroma.weight = 1.05;
         chroma.balance = 1;
         chroma.clipWhite = 1;
@@ -137,8 +137,8 @@ var main = {
 
   playVideo: function() {
 
-    console.log("Starting video.");
-    console.log("Length " + script.popcorn.duration() + ".");
+    //console.log("Starting video.");
+    //console.log("Length " + script.popcorn.duration() + ".");
 
     $("#welcome").fadeOut();
     $("#videoCanvas").fadeIn(); 
